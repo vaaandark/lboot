2a76b4b8466fe75b90c297ffd605a17879b0893a
Wenda Zhang <vaaandark@gmail.com>
2023-09-19 21:41:42 +0800

    test: imporve test script

--- test/qemu_run.sh
+++ test/qemu_run.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
+cd "$(dirname "$0")" || exit 1
+mkdir -p esp/efi/boot
 cp ../target/x86_64-unknown-uefi/debug/lboot.efi esp/efi/boot/bootx64.efi
 qemu-system-x86_64 -enable-kvm \
     -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE.fd \
