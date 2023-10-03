e6d44e34b5015624dbe393d54f7ad63d42a57f9d
Wenda Zhang <vaaandark@gmail.com>
2023-10-03 23:09:38 +0800

    test: support aarch64 tests

--- test/qemu_run.sh
+++ test/qemu_run.sh
@@ -1,26 +1,49 @@
 #!/bin/sh
 set -eux
 
-boot='esp/efi/boot'
-target_dir='../target/x86_64-unknown-uefi/debug'
-default_bz='/boot/vmlinuz-linuz'
+# default options
+target='x86_64-unknown-uefi'
+arch='x86_64'
+qemu_system='qemu-system-x86_64'
 
-efi="${target_dir}/lboot.efi"
-if [ "$#" -eq 1 ] && [ -n "$1" ]; then
-    efi="${target_dir}/examples/$1.efi"
+if [ "$#" -eq 1 ] ; then
+    if [ "$1" = 'arm' ] || [ "$1" = 'arm64' ] || [ "$1" = 'aarch64' ]; then
+        arch='aarch64'
+        target='aarch64-unknown-uefi'
+        qemu_system='qemu-system-aarch64'
+    fi
 fi
+
+boot='esp/efi/boot'
+target_dir="../target/${target}/debug"
+efi="${target_dir}/lboot.efi"
 
 cd "$(dirname "$0")"
 mkdir -p "$boot"
 cp "$efi" "${boot}/bootx64.efi"
-if [ ! -f "${boot}/bzImage.efi" ]; then
-    cp "$default_bz" esp/efi/boot/bzImage.efi
+
+code="ovmf/${arch}/code.fd"
+vars="ovmf/${arch}/vars.fd"
+vars_read_only='on'
+target_specific="-machine q35 -smp 4 -vga std --enable-kvm \
+    -device isa-debug-exit,iobase=0xf4,iosize=0x04"
+if [ "$arch" = 'aarch64' ]; then
+    tmp_var="$(mktemp)"
+    cp "$vars" "$tmp_var"
+    vars="$tmp_var"
+    vars_read_only='off'
+    target_specific="-machine virt -cpu cortex-a72 \
+        -device virtio-gpu-pci"
 fi
 
-qemu-system-x86_64 -enable-kvm \
+"$qemu_system" \
     -m 1G \
     -serial stdio \
-    -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE.fd \
-    -drive if=pflash,format=raw,readonly=on,file=OVMF_VARS.fd \
+    -nodefaults \
+    -device virtio-rng-pci \
+    -boot menu=on,splash-time=0 \
+    $target_specific \
+    -drive if=pflash,format=raw,readonly=on,file="$code" \
+    -drive if=pflash,format=raw,readonly="$vars_read_only",file="$vars" \
     -drive format=raw,file=fat:rw:esp
 
